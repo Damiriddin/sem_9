eb1cc19f155851058bc8e82d656a922bff46626e
Damiriddin <128897262+Damiriddin@users.noreply.github.com>
2023-05-27 22:15:27 +0300

    Update main.c

--- sem9z3/main.c
+++ sem9z3/main.c
@@ -1,11 +1,11 @@
-//Добавим необходимые заголовочные файлы
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 
 int main(int argc, char *argv[]) {
-//получаем аргументы командной строки
+
     char *filename = argv[1];
     int page_length = 2;
     int line_length = 5;
@@ -14,7 +14,6 @@ int main(int argc, char *argv[]) {
     if(argc > 3)
     line_length = atoi(argv[3]);
 
-//открываем файл и считываем его содержимое
 
     FILE *file;
     file = fopen(filename, "r");
@@ -28,7 +27,8 @@ int main(int argc, char *argv[]) {
     int line_count = 0;
     int page_count = 1;
     printf("- Page %d -\n", page_count);
-    // выводим каждую страницу с номерами строк
+    
+    
     while((read = getline(&line, &len, file)) != -1) {
         line_count++;
         if(line_count > page_length) {
