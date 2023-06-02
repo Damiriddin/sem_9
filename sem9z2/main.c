4b08470660437a19d2ad4239b96337cb6888fcb0
Damiriddin <128897262+Damiriddin@users.noreply.github.com>
2023-06-02 22:57:41 +0300

    Update main.c

--- sem9z2/main.c
+++ sem9z2/main.c
@@ -5,7 +5,7 @@
 
 
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) {       // int argc, char *argv[] - аргументы командной строки
 
 setlocale(LC_ALL, "Rus");
 
@@ -13,27 +13,27 @@ setlocale(LC_ALL, "Rus");
     char line1[100], line2[100];
 
   
-    if (argc != 3) {
+    if (argc != 3) {              // проверка
         printf("Usage: file1 file2");
         return 1;
     }
 
    
-    file1 = fopen(argv[1], "r");
+    file1 = fopen(argv[1], "r");  // открываем для чтения
     file2 = fopen(argv[2], "r");
 
     
-    while (fgets(line1, 100, file1) != NULL && fgets(line2, 100, file2) != NULL) {
+    while (fgets(line1, 100, file1) != NULL && fgets(line2, 100, file2) != NULL) {  // считываем и выводим в перемешку до тех пор пока true
         printf("%s%s", line1, line2); 
     }
 
     
-    while (fgets(line1, 100, file1) != NULL) {
+    while (fgets(line1, 100, file1) != NULL) {   // если файлы имеют разное количество строк
         printf("%s", line1); 
     }
 
     
-    while (fgets(line2, 100, file2) != NULL) {
+    while (fgets(line2, 100, file2) != NULL) {    // если файлы имеют разное количество строк
         printf("%s", line2); 
     }
 
