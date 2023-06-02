f78c8959fa1967d737dfb646d9e46c28c7093dd1
Damiriddin <128897262+Damiriddin@users.noreply.github.com>
2023-06-02 22:53:53 +0300

    Update main.c

--- sem9_z1/main.c
+++ sem9_z1/main.c
@@ -6,10 +6,10 @@
 
 void tail(char *filename, int n);
 
-int main(int argc, char *argv[]) {
-if(argc < 2) {
+int main(int argc, char *argv[]) {               // int argc, char *argv[] - аргументы камандн
+if(argc < 2) {                                   // проверка правильности ввода      
 printf("Usage: %s filename [n]\n", argv[0]);
-exit(EXIT_FAILURE);
+exit(1);
 }
 
 
@@ -26,31 +26,31 @@ return 0;
 }
 
 void tail(char *filename, int n) {
-FILE *file = fopen(filename, "r");
+FILE *file = fopen(filename, "r");     // открываем файл
 
 
-if(file == NULL) {
+if(file == NULL) {                      // проверка 
     printf("Error opening file %s\n", filename);
-    exit(EXIT_FAILURE);
+    exit(EXIT_FAILURE);  // EXIT_FAILURE - выход со статусом ошибка 
 }
 
-char lines[n][MAX_LINE_LENGTH];
-int current_line = 0;
-
-while(fgets(lines[current_line % n], MAX_LINE_LENGTH, file) != NULL) {
+char lines[n][MAX_LINE_LENGTH];   
+int current_line = 0;    
+                                    
+while(fgets(lines[current_line % n], MAX_LINE_LENGTH, file) != NULL) {   // считываем строки в line[][]
     current_line++;
 }
 
 int i;
-int start = current_line - n;
-if(start < 0) {
-    start = 0;
+int start = current_line - n;       
+if(start < 0) {                              
+    start = 0;                   
+}
+ 
+for(i = start; i < current_line; i++) {             // выводим
+    printf("%s", lines[i % n]);         
 }
 
-for(i = start; i < current_line; i++) {
-    printf("%s", lines[i % n]);
-}
-
-fclose(file);
+fclose(file);    // закрываем
 }
 
