781e5cc2b317d84183d0922df94ccf30a08fee67
Damiriddin <128897262+Damiriddin@users.noreply.github.com>
2023-06-12 17:57:21 +0300

    Update main.c

--- sem9z3/main.c
+++ sem9z3/main.c
@@ -1,7 +1,8 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <iso646.h>
+// изменения добавлены проверки аргументов командной строки
 
 // функция read_string считывает символы из file и сохраняет в buffer размером n 
 void read_string(FILE *file, char *buffer, int n) { 
@@ -22,11 +23,39 @@ void read_string(FILE *file, char *buffer, int n) {
 }
 
 int main(int argc, char **argv) {
+    
+    // проверка количества аргуметов камандной строки
+    if (argc != 6){
+        printf("Incorect number of arguments\n");
+        puts("Exemple: .\\main.exe file.txt -p 2 -n 5 ");
+        exit(1);
+    }
+
     char *buffer; 
     int p = 2, n = 5, page = 0, not_done = 1, written = 0; // p - количество строк на одной стронице, n - максимальная длина строки на страницу 
     FILE *file;
     file = fopen(argv[1], "r");
     
+    //  проверка файла 
+    if (file ==NULL){
+        puts("Invalid openning a file");
+        puts("Exemple: .\\main.exe file.txt -p 2 -n 5");
+        exit(1);
+    }
+
+
+// проверка опций -n и -p 
+if (!(strcmp(argv[2],"-p") == 0 and atoi(argv[3]))){
+    puts("Invalid input of the -p option or its values\n\
+    Exemple: .\\main.exe file.txt -p 2 -n 5");
+    exit(1);
+}
+if (!(strcmp(argv[4],"-n") == 0 and atoi(argv[5]))){
+    puts("Invalid input of the -n option or its values\n\
+    Exemple: .\\main.exe file.txt -p 2 -n 5");
+    exit(1);
+}
+
     // проверка агрументов командной строки 
     for (int i = 2; i <= argc-2; i++) {
         if (!strcmp(argv[i], "-p"))
@@ -34,6 +63,13 @@ int main(int argc, char **argv) {
         if (!strcmp(argv[i], "-n")) 
             n = atoi(argv[i+1])+1;
     }
+    if (p <= 0 || n <= 0) {
+    printf("Invalid arguments\n");
+    exit(1);
+    }
+    
+
+
     // выделение памяти по буфер для чтения строк
     buffer = (char *)malloc(n * sizeof(char));
     if ((p == 0) || (n == 1)) {  // проверка ввода 
